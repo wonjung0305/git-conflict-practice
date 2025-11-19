e22c9332706af9c692800bbccccce6b578a5bc43
eunwoo814 <petit10082@gmail.com>
2025-11-19 20:38:19 +0900

    commit

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 void performCalculation(int x, int y);
 void handleUserInput();
 void displayResult(int result);
@@ -9,10 +10,9 @@ int main()
   handleUserInput();
   return 0;
 }
-void performCalculation(int x, int y)
-{
-  printf("Calculating result for %d and %d...\n", x, y);
-  int result = x + y;
+void performCalculation(int x, int y) {
+  printf("Calculating %d to the power of %d...\n", x, y);
+  int result = pow(x, y);
   displayResult(result);
 }
 void handleUserInput()

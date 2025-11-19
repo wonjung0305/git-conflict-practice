1e11719b33f348037d6c3cdfdb69f4579ac47e6c
Hansol916 <hansol2467@handong.ac.kr>
2025-11-19 20:33:21 +0900

    first commit

--- main.c
+++ main.c
@@ -24,5 +24,7 @@ void handleUserInput()
 }
 void displayResult(int result)
 {
-  printf("The result is: %d\n", result);
+  printf("**********\n");
+  printf("The computed result is: %d\n", result);
+  printf("**********\n");
 }
